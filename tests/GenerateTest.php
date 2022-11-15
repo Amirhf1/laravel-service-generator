07139794eff27d6c8ba3cb6e369e0680d65edb77
Amir Hossein Fallah <a.fallah@adam-barfi.ir>
2022-11-16 00:48:21 +0330

    create empty test class

---
+++ tests/GenerateTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Amirhf\LaravelServiceGenerator\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class GenerateTest extends TestCase
+{
+
+}
---
+++ tests/StubExistTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Amirhf\LaravelServiceGenerator\Tests;
+
+class StubExistTest
+{
+
+}
