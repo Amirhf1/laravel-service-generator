659ad11cc9f59b89db81fadfda33b1add69a74c0
Amir Hossein Fallah <a.fallah@adam-barfi.ir>
2022-11-16 00:47:08 +0330

    improve LaravelServiceGeneratorFacade.php

--- src/LaravelServiceGeneratorFacade.php
+++ src/LaravelServiceGeneratorFacade.php
@@ -10,11 +10,9 @@ use Illuminate\Support\Facades\Facade;
 class LaravelServiceGeneratorFacade extends Facade
 {
     /**
-     * Get the registered name of the component.
-     *
      * @return string
      */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'laravel-service-generator';
     }
